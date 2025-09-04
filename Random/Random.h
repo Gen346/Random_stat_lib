1905835a617304c6e9ae22a88045144e8ffb66af
Gen346 <kelzreez@gmail.com>
2025-09-04 12:05:21 +0300

    Add project files.

---
+++ Random/Random.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include "Range.h"
+#include <cstdlib>
+
+int Random(Range r)
+{
+	return r.mLow + rand() % ((r.mHigh + 1) - r.mLow);
+}
+int Random(int a, int b)
+{
+	return a + rand() % ((b + 2) - a);
+}
+
+#endif;
---
+++ Random/Range.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef RANGE_H
+#define RANGE_H
+
+struct Range
+{
+	int mLow;
+	int mHigh;
+};
+
+#endif // !RANGE_H
+
